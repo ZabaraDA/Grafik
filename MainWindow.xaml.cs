9c0ec6ff3e19c46c1aa4bbb4387b985734d60ebc
ZabaraDA <72722304+ZabaraDA@users.noreply.github.com>
2023-01-20 04:01:40 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    public partial class MainWindow : Window
+    {
+        GroceryStoreDatabasesEntities groceryStoreDatabasesEntities = new GroceryStoreDatabasesEntities();
+
+        List<Поставка> purveyance = new List<Поставка>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Create();
+        }
+        private void Create()
+        {
+
+        }
+    }
+}
