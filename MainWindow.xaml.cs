9fb89220984ea09534fda2400b886e4cf7d84d69
ZabaraDA <72722304+ZabaraDA@users.noreply.github.com>
2023-01-21 11:24:41 +0300

    surface

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,17 +20,65 @@ namespace WpfApp8
     {
         GroceryStoreDatabasesEntities groceryStoreDatabasesEntities = new GroceryStoreDatabasesEntities();
 
-        List<Поставка> purveyanceProductList = new List<Поставка>();
+        List<Поставка> purveyanceProductList = new List<Поставка>()
+        {
+            
+        };
         List<Списание> withdrawalProductList = new List<Списание>();
+
+        int quantity = 10;
+        int indent = 50;
+
 
         public MainWindow()
         {
             InitializeComponent();
-            Create();
+            CreateChart();
         }
-        private void Create()
+        private void CreateChart()
         {
 
+            for (int i = 0; i < quantity; i++)
+            {
+                TextBlock vertical = new TextBlock() 
+                {
+                    Text = (quantity - i).ToString(),
+                    RenderTransform = new TranslateTransform(indent, indent * i),
+                    FontSize = 24,
+                };
+                TextBlock horizontal = new TextBlock()
+                {
+                    Text = (i+1).ToString(),
+                    RenderTransform = new TranslateTransform(indent * (i+2), indent * quantity),
+                    FontSize = 24,
+                };
+                Line line = new Line()
+                {
+                    X1 = indent,
+                    X2 = indent * (i + 2),
+                    Y1 = indent * i,
+                    Y2 = indent * quantity,
+                    Fill = Brushes.Black,
+                    StrokeThickness = 1,
+                    Stroke = Brushes.Black
+                };
+                ChartCanvas.Children.Add(vertical);
+                ChartCanvas.Children.Add(horizontal);
+                ChartCanvas.Children.Add(line);
+            }
+
+
+            for (int i = 0; i < purveyanceProductList.Count; i++)
+            {
+                Rectangle rectangle = new Rectangle()
+                {
+                    Width = 50,
+                    Height = 100,
+                    Fill = Brushes.Green,
+                    RenderTransform = new TranslateTransform(30, 30),
+                };
+                ChartCanvas.Children.Add(rectangle);
+            }
         }
     }
 }
