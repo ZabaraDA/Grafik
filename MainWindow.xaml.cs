ccce1757ed9019b52e26dbe47d042055a25b659f
ZabaraDA <72722304+ZabaraDA@users.noreply.github.com>
2023-01-29 23:05:58 +0300

    534

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,11 +20,6 @@ namespace WpfApp8
     public partial class MainWindow : Window
     {
         GroceryStoreDatabasesEntities groceryStoreDatabases = new GroceryStoreDatabasesEntities();
-
-        List<Поставка> purveyanceProductList = new List<Поставка>()
-        {
-        };
-        List<Списание> withdrawalProductList = new List<Списание>();
 
         DateTime month = DateTime.Now;
 
@@ -40,11 +36,14 @@ namespace WpfApp8
             ProductComboBox.SelectedIndex = 0;
             CreateRangeYears();
             CreateChart();
+            ChartCanvas1.Width = ChartCanvas.Width;
+            ChartCanvas1.Height = ChartCanvas.Height;
 
         }
         private void CreateChart()
         {
             ChartCanvas.Children.Clear();
+            ChartCanvas1.Children.Clear();
 
             Товар selectedProduct = groceryStoreDatabases.Товар.Where(x => x.Наименование.Equals(ProductComboBox.Text)).FirstOrDefault();
             List<ТоварПоставка> deliverySelectedProductList = groceryStoreDatabases.ТоварПоставка.Where(x => x.КодТовара.Equals(selectedProduct.Код)).ToList();
@@ -52,9 +51,16 @@ namespace WpfApp8
             {
                 return;
             }
+
+            List<Поставка> deliveryList = groceryStoreDatabases.Поставка.ToList().Where(x => x.ДатаПоставки.Year.Equals(Convert.ToInt16(ShowYearComboBox.Text))).ToList();
+            //List<Поставка> deliveryProductList = new List<Поставка>();
+            //for (int i = 0; i < deliverySelectedProductList.Count; i++)
+            //{
+            //    deliveryProductList.Add(deliveryList.Where(x => x.Код.Equals(deliverySelectedProductList[i].КодПоставки)).FirstOrDefault());
+            //}
+
             int max = deliverySelectedProductList.Max(x => x.Количество);
             int step = 1;
-            int f = deliverySelectedProductList.Max(x => x.Количество);
             while (max >10)
             {
                 max = max / 10;
@@ -63,8 +69,7 @@ namespace WpfApp8
             step = step / 10;
             int quantityInSegment = max*step;
 
-            List<Поставка> deliveryList = groceryStoreDatabases.Поставка.ToList().Where(x => x.ДатаПоставки.Year.Equals(Convert.ToInt16(ShowYearComboBox.Text))).ToList();
-
+            GenerateCanvas();
             for (int i = 0; i < quantity; i++)
             {
                 TextBlock vertical = new TextBlock()
@@ -76,6 +81,82 @@ namespace WpfApp8
                     TextAlignment = TextAlignment.Right,
                     Width = 95,
                 };
+                ChartCanvas.Children.Add(vertical);
+            }
+           
+           
+
+            for (int i = 0; i < deliveryList.Count; i++)
+            {
+                InfoTextBlock.Text = step.ToString();
+                
+                
+                var b = deliverySelectedProductList.Where(x => x.КодПоставки.Equals(deliveryList[i].Код)).FirstOrDefault();
+                if(b == null)
+                {
+                    MessageBox.Show("Внимание");
+                    return;
+                }
+
+               
+                StackPanel grid = new StackPanel()
+                {
+                    Orientation = Orientation.Horizontal,
+                    RenderTransform = new TranslateTransform(
+                        indient * (deliveryList[i].ДатаПоставки.Month)* 0 
+                                                                          ,
+
+
+                    ((indient * quantity) - b.Количество * indient / quantityInSegment)* 0 ) ,
+
+                    ToolTip = $"Доставлено{b.Количество}\\ {selectedProduct.Наименование}\n Остаток с поставки {b.Остаток}",
+                    VerticalAlignment = VerticalAlignment.Bottom,
+                    Background = Brushes.Tan,
+                    //LayoutTransform = new RotateTransform(180, -indient * (deliveryList[i].ДатаПоставки.Month - quantity),
+                    //  -(quantity) * indient),
+
+                };
+                Rectangle rectangle = new Rectangle()
+                {
+                    Width = indient,
+                    Height = b.Количество * indient/ quantityInSegment,
+                    Fill = Brushes.Violet,
+                    VerticalAlignment = VerticalAlignment.Bottom,
+
+                    //RenderTransform = new TranslateTransform(0, (b.Количество * indient / quantityInSegment)),
+                    LayoutTransform = new RotateTransform(180, 0, -(b.Количество * indient / quantityInSegment))
+                };
+                Rectangle rectangle1 = new Rectangle()
+                {
+                    Width = indient,
+                    Height = b.Остаток * indient / quantityInSegment,
+                    Fill = Brushes.DarkCyan,
+                    VerticalAlignment = VerticalAlignment.Top,
+                };
+                rectangle1.LayoutTransform = new RotateTransform(180,0, -(b.Остаток * indient / quantityInSegment));
+
+                    //(rectangle.Height - rectangle1.Height));
+
+                InfoTextBlock.Text = grid.RenderTransform.Value.ToString();
+
+                DoubleAnimation buttonAnimation = new DoubleAnimation();              
+                buttonAnimation.From =0;
+                buttonAnimation.To = b.Количество * indient / quantityInSegment;          
+                buttonAnimation.Duration = TimeSpan.FromSeconds(3);
+
+                rectangle.BeginAnimation(HeightProperty, buttonAnimation);
+                buttonAnimation.To = b.Остаток * indient / quantityInSegment;
+                rectangle1.BeginAnimation(HeightProperty, buttonAnimation);
+                grid.Children.Add(rectangle);
+                grid.Children.Add(rectangle1);
+                ChartCanvas1.Children.Add(grid);
+            }
+        }
+
+        private void GenerateCanvas()
+        {
+            for (int i = 0; i < quantity; i++)
+            {
 
                 TextBlock horizontal = new TextBlock()
                 {
@@ -89,7 +170,7 @@ namespace WpfApp8
                 };
                 Line horizontalLine = new Line()
                 {
-                    X1 = indient,
+                    X1 = indient*0,
                     X2 = indient * quantity * 2 + indient * 2,
                     Y1 = indient * i + indient,
                     Y2 = indient * i + indient,
@@ -109,53 +190,16 @@ namespace WpfApp8
                     Stroke = Brushes.Firebrick
                 };
 
-                ChartCanvas.Children.Add(vertical);
+               
                 ChartCanvas.Children.Add(horizontal);
                 ChartCanvas.Children.Add(horizontalLine);
                 ChartCanvas.Children.Add(verticalLLine);
 
             }
-
-
-            for (int i = 0; i < deliveryList.Count; i++)
-            {
-                InfoTextBlock.Text = step.ToString();
-                var b = deliverySelectedProductList.Where(x => x.КодПоставки.Equals(deliveryList[i].Код)).FirstOrDefault();
-                StackPanel grid = new StackPanel()
-                {
-                    Orientation = Orientation.Horizontal,
-                    RenderTransform = new TranslateTransform(indient * (13 - deliveryList[i].ДатаПоставки.Month) * 2,(quantityInSegment*(quantity-2))- b.Количество * indient / quantityInSegment),
-                    ToolTip = $"Доставлено{b.Количество}\\ {selectedProduct.Наименование}\n Остаток с поставки {b.Остаток}",
-                    //VerticalAlignment = VerticalAlignment.Bottom,
-                    
-                };
-                Rectangle rectangle = new Rectangle()
-                {
-                    Width = indient,
-                    Height = b.Количество * indient/ quantityInSegment,
-                    Fill = Brushes.Violet,
-                    VerticalAlignment = VerticalAlignment.Top
-                };
-                Rectangle rectangle1 = new Rectangle()
-                {
-                    Width = indient,
-                    Height = b.Остаток* indient / quantityInSegment,
-                    Fill = Brushes.DarkCyan,
-                    VerticalAlignment = VerticalAlignment.Top
-                };
-                rectangle1.RenderTransform = new TranslateTransform(0, rectangle.Height - rectangle1.Height);
-                InfoTextBlock.Text = grid.RenderTransform.Value.ToString();
-
-                grid.Children.Add(rectangle);
-                grid.Children.Add(rectangle1);
-                ChartCanvas.Children.Add(grid);
-            }
         }
 
         private void CreateRangeYears()
         {
-            
-
             DateTime firstDeliveryDate = groceryStoreDatabases.Поставка.Min(x => x.ДатаПоставки);
             int timeRange = month.Year - firstDeliveryDate.Year;
             InfoTextBlock.Text = timeRange.ToString();
@@ -171,11 +215,20 @@ namespace WpfApp8
         private void ProductComboBox_DropDownClosed(object sender, EventArgs e)
         {
             CreateRangeYears();
+            CreateChart();
         }
 
         private void ShowYearComboBox_DropDownClosed(object sender, EventArgs e)
         {
             CreateChart();
+        }
+
+        private void ChartCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+           int xMousePosition = Convert.ToInt16(e.GetPosition(ChartCanvas1).X);
+           int  yMousePosition = Convert.ToInt16(e.GetPosition(ChartCanvas1).Y);
+            txtbX.Text = xMousePosition.ToString();
+            txtbY.Text = yMousePosition.ToString();
         }
     }
 }
